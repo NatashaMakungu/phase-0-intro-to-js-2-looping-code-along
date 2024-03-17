58f6f33c2b05f17cbab5d40c26f2a2905768ad73
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-03-17 17:53:53 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const eventName = ["birthday"];
+
+function writeCards(names, eventName) {
+    let messages = []; 
+    for (let i = 0; i < names.length; i++) {
+      
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message); 
+      debugger;
+    }
+    return messages; 
+}
+  // Example usage:
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+
+  
+
+    
+  
+  
+  
+  
+  
